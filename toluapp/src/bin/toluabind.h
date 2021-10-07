dae75c91b253eda1fa64b567b04a5a5cda687cd5
pengpenglang <pengpenglang@126.com>
2021-10-07 14:07:04 +0800

    update

--- toluapp/src/bin/toluabind.h
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: tolua
-** Generated automatically by tolua++-1.0.92 on Mon Feb  1 16:14:01 2021.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
-
